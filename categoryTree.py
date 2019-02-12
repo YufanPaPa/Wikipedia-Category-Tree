802afd08231ec98a0fadf6b0ae33e2a16bc415e6
YUFAN <506075305@qq.com>
2019-02-12 15:51:45 +0900

    modified_form

--- categoryTree.py
+++ categoryTree.py
@@ -1,44 +1,60 @@
+#coding=utf-8
+
 from wikitools import wiki,category,api
 import sys
+import chardet
 
-def wikipedia_query(query_params,lang='en'):
-	site = wiki.Wiki(url='http://'+lang+'.wikipedia.org/w/api.php')
+def wikipedia_query(query_params,lang='ja'):
+	site = wiki.Wiki(url='https://'+lang+'.wikipedia.org/w/api.php')
 	request = api.APIRequest(site, query_params)
 	result = request.query(False)
 	return result[query_params['action']]
 
-def get_category_members(par_category, depth, lang='en'):
+def get_category_members(par_category, depth, index, lang='ja'):
     category_name = 'Category:'+par_category
     if depth < 0:
         return 0
-    
     #Begin crawling articles in category
-    results = wikipedia_query({'list': 'categorymembers','cmtitle': category_name,'cmtype': 'page','cmlimit': '500','action': 'query'},lang)  
+    results_pages = wikipedia_query({'list': 'categorymembers','cmtitle': category_name,'cmtype': 'page','cmlimit': '500','action': 'query'},lang)  
+    
     
     # Begin crawling subcategories
-    results = wikipedia_query({'list': 'categorymembers',
+    results_cate = wikipedia_query({'list': 'categorymembers',
                                    'cmtitle': category_name,
                                    'cmtype': 'subcat',
                                    'cmlimit': '500',
-                                   'action': 'query'},lang)
+                                   'action': 'query',
+                                   'format': 'json'},lang)
+    
     subcategories = []
-    if 'categorymembers' in results.keys() and len(results['categorymembers']) > 0:
-        for i, category in enumerate(results['categorymembers']):
+    pages = []
+    if 'categorymembers' in results_cate.keys() and len(results_cate['categorymembers']) > 0:
+        for i, category in enumerate(results_cate['categorymembers']):
             cat_title = category['title']
             subcategories.append(cat_title.strip('Category:'))
-    subcat= ''.join(cat.encode('latin-1', 'ignore')+',' for cat in subcategories).strip(',')
-    if subcat !='':
-      wp.write(par_category.encode('latin-1', 'ignore')+':'+subcat+'\n')
+        subcat= ''.join(cat+'+' for cat in subcategories).strip('+')
+    elif 'categorymembers' in results_pages.keys() and len(results_pages['categorymembers']) > 0:
+        for i, category in enumerate(results_pages['categorymembers']):
+            cat_title = category['title']
+            pages.append(cat_title.strip('Category:'))
+        subcat= ''.join(cat+'+' for cat in pages).strip('+')
+        if subcat !='':
+            wp.write(index+':'+subcat.encode("utf-8")+'\n')
+
+    else:
+        subcat = ''
     for category in subcategories:
-      get_category_members(category,depth-1)
+        index_ = index+">"+category.encode("utf-8")
+        get_category_members(category.encode("utf-8"),depth-1,index_)
 
-
-wikisite = "http://en.wikipedia.org/w/api.php"
+wikisite = "https://ja.wikipedia.org/w/api.php"
 par_category = sys.argv[1]
 depth =int(sys.argv[2])
 cat_tree =sys.argv[3]
 wp =open(cat_tree,'w')
+total_item=0
+index = par_category
 
 
-get_category_members(par_category,depth,'en')
+get_category_members(par_category,depth,index,'ja')
 
